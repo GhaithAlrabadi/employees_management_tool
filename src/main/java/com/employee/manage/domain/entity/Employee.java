a119daa2eb880b2450ce58cf0a2c7e2e6fda2473
unknown <ghaithnet2004@gmail.com>
2023-06-11 22:58:02 -0500

    first time domain change

--- src/main/java/com/employee/manage/domain/entity/Employee.java
+++ src/main/java/com/employee/manage/domain/entity/Employee.java
@@ -1,4 +1,41 @@
 package com.employee.manage.domain.entity;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private Address address;
+    private JobDetails jobDetails;
+
+    public Employee(String firstName, String lastName, Address address, JobDetails jobDetails) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.jobDetails = jobDetails;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public JobDetails getJobDetails() {
+        return jobDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address=" + address +
+                ", jobDetails=" + jobDetails +
+                '}';
+    }
 }
